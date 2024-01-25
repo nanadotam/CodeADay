208b8876acd35a9c4f09d9758daf4e4e2e092c44
Nana Amoako <63524504+nanadotam@users.noreply.github.com>
2024-01-25 05:13:21 +0000

    Day 2

---
+++ Day2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Day2 {
+
+    
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input the year: ");
+        int year = in.nextInt();
+
+        boolean x = (year % 4) == 0;
+        boolean y = (year % 100) != 0;
+        boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+        if (x && (y || z))
+        {
+            System.out.println(year + " is a leap year");
+        }
+        else
+        {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
