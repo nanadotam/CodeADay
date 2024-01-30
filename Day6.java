6330830c5215192af50c29f42fde1eeba4b93ca5
Nana Amoako <63524504+nanadotam@users.noreply.github.com>
2024-01-30 00:10:13 +0000

    Day 6

---
+++ Day6.java
@@ -0,0 +1,6 @@
+/*
+Write a Java program to take the user for a distance (in meters) 
+and the time taken (as three numbers: hours, minutes, seconds), 
+and display the speed, in meters per second, kilometers per hour
+and miles per hour (hint: 1 mile = 1609 meters).
+*/
