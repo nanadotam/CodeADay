cbd64179a1a039c3bf719cacca34ff37e5ca66fe
Nana Amoako <63524504+nanadotam@users.noreply.github.com>
2024-02-23 19:41:53 +0000

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,18 @@
+// Late
+
+public class Main {
+public static void main (String[] args){
+Days_Of_Week today = Days_of_week.FRIDAY;
+system.out.println("Today is " + today);
+}
+
+public enum   Days_Of_Week {
+  SUNDAY,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY, 
+  FRIDAY,
+  SATURDAY
+}
+}
